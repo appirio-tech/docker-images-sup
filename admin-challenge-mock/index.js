b10465ac933797a4399b4646c78796f0ac2530a3
Parth Shah <parth.shah@appirio.com>
2016-01-21 11:20:22 -0800

    switching with express server

--- admin-challenge-mock/index.js
+++ admin-challenge-mock/index.js
@@ -1,75 +1,119 @@
-var express = require('express');
-var jsonServer = require('json-server');
-var bodyParser = require('body-parser');
 
-var PORT = process.argv[2] || 3000;
-var dbPath = process.argv[3] || "db.json";
-// APP
+var fs = require('fs');
+var data = JSON.parse(fs.readFileSync('db.json', 'utf8'));
 
-// Returns an Express server
-var server = jsonServer.create();
-server.use(bodyParser.json());
+var return404 = function() {
+  return {
+    "id": "-d1eec1c:151c946bf36:-5d5f",
+    "result": {
+      "success": true,
+      "status": 404,
+      "metadata": null,
+      "content": "Resource Not found"
+    },
+    "version": "v3"
+  };
+}
 
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(jsonServer.defaults())
-var router = jsonServer.router(dbPath);
-// wrap in content property
-router.render = function(req, res) {
-  console.log(res.statusCode)
-
-  if (res.statusCode === 200) {
-    if (req.path.indexOf("members") > 0) {
-      delete res.locals.data.id;
-    }
-    res.jsonp({
+var returnData = function(data) {
+    return {
       "id": "23bd6312:14e1d778bb9:-7fff",
       "result": {
         "success": true,
         "status": 200,
         "metadata": null,
-        "content": res.locals.data
+        "content": data
       }
-    });
-  } else {
-    res.json({
-      "id": "-d1eec1c:151c946bf36:-5d5f",
-      "result": {
-        "success": true,
-        "status": 404,
-        "metadata": null,
-        "content": "Resource Not found"
-      },
-      "version": "v3"
-    })
+    };
   }
+  // server.js
 
-};
+// BASE SETUP
+// =============================================================================
 
-//handle v3/ files endpoint
-server.post('/v3/files/uploadurl', function(req, res) {
+// call the packages we need
+var express = require('express'); // call express
+var app = express(); // define our app using express
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080; // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router(); // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
   res.json({
-    "id": "23bd6312:14e1d778bb9:-7fff",
-    "result": {
-      "success": true,
-      "status": 200,
-      "metadata": null,
-      "content": {
-        "filePath": req.body.param.filePath,
-        "preSignedURL": "https://sandbox-temp-bucket.s3.amazonaws.com/files/" + req.body.param.filePath,
-        "contentType": req.body.param.contentType,
-        "public": true
-      }
-    },
-    "version": "v3"
+    message: 'hooray! welcome to our api!'
   });
 });
+router.get('/members/', function(req, res) {
+  res.json(data.members);
+});
 
-server.use('/v3/', router)
-server.use(jsonServer.rewriter({
-  '/members/:handle/:resource/?filter=id%3D:id': '/:resource/:id'
-}));
+router.get('/users/', function(req, res) {
+  res.json(data.users);
+});
 
-console.log("Starting Mock Server.... ");
-console.log("PORT: " + PORT);
+router.get('/challenges/', function(req, res) {
+  res.json(data.challenges);
+});
 
-server.listen(PORT);
+router.get('/members/:handle', function(req, res) {
+  console.log(req.params);
+  var member = _.find(data.members, function(m) {return req.params["handle"] === m.handle});
+  if (member) {
+    res.json(returnData(member));
+  } else {
+    res.json(return404());
+  }
+})
+
+router.get('/challenges/:id', function(req, res) {
+  var member = _.find(data.challenges, function(m) {return req.params["id"] == m.id});
+  if (member) {
+    res.json(returnData(member));
+  } else {
+    res.json(return404());
+  }
+})
+
+router.get('/users/:id', function(req, res) {
+  var member = _.find(data.users, function(m) {return req.params["id"] === m.id});
+  if (member) {
+    res.json(returnData(member));
+  } else {
+    res.json(return404());
+  }
+})
+
+router.get('/members/:handle/challenges/', function(req, res) {
+  console.log(req.query);
+  var cid =req.query.filter.split("=")[1];
+  console.log(cid);
+  var member = _.find(data.challenges, function(m) {return cid == m.id});
+  if (member) {
+    res.json(returnData(member));
+  } else {
+    res.json(return404());
+  }
+})
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/v3', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
